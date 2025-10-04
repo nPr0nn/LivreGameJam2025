41d4b558e9ae2f742e9cca7d4243866073f13eb3
Nícolas Hecker Silva <nicolashecker.nhs@gmail.com>
2025-10-04 11:22:00 -0300

    sei la

---
+++ src/character.h
@@ -0,0 +1,15 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+typedef struct {
+    Vector2 pos;
+    float radius;
+    Color color;
+    float velocity_y;
+} Character;
+
+void character_init(Character *ch, Vector2 start_pos, float radius, Color color);
+void character_update(Character *ch);
+void character_draw(const Character *ch);
+
+#endif // CHARACTER_H
